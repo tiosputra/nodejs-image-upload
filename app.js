862df08cd7f2b82b6839bb6f9bc4da2aa2e2b745
karyaTio <karya.tiosaputra@gmail.com>
2020-03-06 18:42:38 +0700

    Push

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const multer = require("multer");
+const ejs = require("ejs");
+const path = require("path");
+
+// Set storage engine
+const storage = multer.diskStorage({
+  destination: "./public/uploads/",
+  filename: function(req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1000000 },
+  fileFilter: function(req, file, cb) {
+    checkFileType(file, cb);
+  }
+}).single("myImage");
+
+// Check file Type
+function checkFileType(file, cb) {
+  // Allowed ext
+  const fileTypes = /jpeg|jpg|png|gif/;
+  // Check ext
+  const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimeType = fileTypes.test(file.mimetype);
+
+  if (mimeType && extName) {
+    return cb(null, true);
+  } else {
+    cb("Error: Images Only !!!");
+  }
+}
+
+const app = express();
+
+app.post("/upload", (req, res) => {
+  upload(req, res, err => {
+    if (err) {
+      res.render("index", { msg: err });
+    } else {
+      if (req.file == undefined) {
+        res.render("index", { msg: "Error : No File Selected!" });
+      } else {
+        res.render("index", {
+          msg: "File Uploaded!",
+          file: `uploads/${req.file.filename}`
+        });
+      }
+    }
+  });
+});
+
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
